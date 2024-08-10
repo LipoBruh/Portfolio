aaf6a052f3d2d01e6cbec1d5b9e39f6941950517
LipoBruh <lipo.bruh@gmail.com>
2024-08-10 17:21:23 -0400

    Minor style change and redeploy

--- my-react-app/src/components/CategoryPicker.jsx
+++ my-react-app/src/components/CategoryPicker.jsx
@@ -3,6 +3,8 @@ import React, { useState,useEffect } from 'react';
 
 export const CategoryPicker = ({ setCategory }) => {
 
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 768);
+
     useEffect(() => {
         handleCategoryClick("3D Modeling")
       }, []); 
@@ -48,13 +50,25 @@ export const CategoryPicker = ({ setCategory }) => {
 
 
 const Category = ({ category, isToggled, onClick }) => {
+
+  //responsive font resizer
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 768);
+
+  //will add an event listener on first render
+  useEffect(() => {
+    const handleResize = () => {setIsWideScreen(window.innerWidth > 1000);};
+    window.addEventListener('resize', handleResize); //will update the hook upon trigger
+    //
+    return () => {window.removeEventListener('resize', handleResize);};
+  }, []);
+
   return (
     <div
       className={`col-2 clickable ${isToggled ? "border-bottom" : ""}`}
       onClick={onClick}
     >
       <div className='row align-items-center'>
-        <div className='col' style={{fontFamily: "JetBrains", "fontSize":"1.2rem",overflowWrap: "break-word"}}>{category}</div>
+        <div className='col' style={{fontFamily: "JetBrains", "fontSize":`${isWideScreen ? "1.1rem" : "0.8rem"}`,overflowWrap: "break-word"}}>{category}</div>
       </div>
     </div>
   );
