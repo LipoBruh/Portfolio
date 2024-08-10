80bab064d0ba5045854e3cdda88a500afb8154a1
LipoBruh <lipo.bruh@gmail.com>
2024-08-10 16:57:08 -0400

    Rebuilding production code

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   root: './',  // Set the root directory
   build: {
     outDir: '../docs',  // Ensure the output directory is 'build'
